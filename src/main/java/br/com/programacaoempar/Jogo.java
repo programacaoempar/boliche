afeb9a1818f4ce188a645967473ab717b5bff12c
Ivan Sandrini <ivansandrini@gmail.com>
2022-07-07 11:54:20 -0300

    Uma rapida brincada

--- src/main/java/br/com/programacaoempar/Jogo.java
+++ src/main/java/br/com/programacaoempar/Jogo.java
@@ -1,32 +1,23 @@
 package br.com.programacaoempar;
 
 public class Jogo {
-
-
-    int[] pontuacaoTodasJogadas = new int[20];
-    int jogada = 0;
-    public void executaJogada(int quedaPinos) {
-        if (eStrike(quedaPinos)){
-            pontuacaoTodasJogadas[jogada++] = quedaPinos;
-            pontuacaoTodasJogadas[jogada] = 0;
-            jogada++;
-        }else{
-            pontuacaoTodasJogadas[jogada++] = quedaPinos;
-        }
+    private int[] pontuacaoTodasJogadas = new int[21];
+    private int jogadaAtual = 0;
+    public void executaJogada(int quantidadeDePinusDerrubado) {
+        pontuacaoTodasJogadas[jogadaAtual++] = quantidadeDePinusDerrubado;
     }
 
-    public int pontuacao() {
+    public int obtemPontuacao() {
         int pontuacao = 0;
         int indiceRodada = 0;
         for (int rodadas = 0; rodadas < 10; rodadas++) {
-            if (eStrike(indiceRodada)){ //strike
-                pontuacao += 10 + pontuacaoTodasJogadas[indiceRodada+2] + pontuacaoTodasJogadas[indiceRodada+3];
-                indiceRodada += 2;
-            }
-            else if (eSpare(indiceRodada)) { //spare
+            if (eUmStrike(indiceRodada)){ //strike
+                pontuacao += 10 + pontuacaoTodasJogadas[indiceRodada+1] + pontuacaoTodasJogadas[indiceRodada+2];
+                indiceRodada++;
+            } else if (eUmSpare(indiceRodada)) { //spare
                 pontuacao += 10 + pontuacaoTodasJogadas[indiceRodada+2];
                 indiceRodada += 2;
-            }else{
+            } else{
                 pontuacao += pontuacaoTodasJogadas[indiceRodada] + pontuacaoTodasJogadas[indiceRodada+1];
                 indiceRodada += 2;
             }
@@ -34,11 +25,11 @@ public class Jogo {
         return pontuacao;
     }
 
-    private boolean eSpare(int i) {
+    private boolean eUmSpare(int i) {
         return pontuacaoTodasJogadas[i] + pontuacaoTodasJogadas[i + 1] == 10;
     }
 
-    private boolean eStrike(int indiceRodada) {
+    private boolean eUmStrike(int indiceRodada) {
         return pontuacaoTodasJogadas[indiceRodada] == 10;
     }
 }
